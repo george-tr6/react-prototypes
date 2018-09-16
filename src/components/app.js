559cc03b5fa6f67dd4aa231e6b67b94a3937a954
george-tr6 <george.tr6@gmail.com>
2018-09-15 23:51:03 -0700

    functional components completed

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,32 @@
+import React from 'react';
+import Table from './table';
+
+export default () => {
+
+    const students = [
+        {
+            name: 'Peter Parker',
+            course: 'Intro to Photography',
+            grade: 85
+        },
+        {
+            name: 'Bruce Banner',
+            course: 'Psychology',
+            grade: 91
+        },
+        {
+            name: 'Tony Stark',
+            course: 'Mechanical Engineering',
+            grade: 100
+        }
+    ];
+
+    return (
+        <div className="container">
+            <h1>Student Grade Table</h1>
+            <Table data={students}/>
+        </div>
+    )
+}
 
 
