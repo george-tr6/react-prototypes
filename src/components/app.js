0332f486b112263fb77da9edf9b88a48da5dc526
george-tr6 <george.tr6@gmail.com>
2018-09-17 18:10:02 -0700

    completed feature set 3

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,13 @@
+import React from 'react';
+import ContactList from './contact_list';
 
-
+export default () => {
+    return (
+        <div className="container">
+            <h1 className="text-center my-3">Address Book</h1>
+            <div className="row">
+                <ContactList/>
+            </div>
+        </div>
+    )
+}
