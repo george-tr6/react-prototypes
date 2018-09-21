08be2b696f57e3f9ffa0620930cfb66931d1140f
george-tr6 <george.tr6@gmail.com>
2018-09-20 18:49:59 -0700

    scrapbook completed

---
+++ src/components/image_data.js
@@ -0,0 +1,73 @@
+import birds from './images/birds.jpeg';
+import bird2 from './images/bird2.jpeg';
+import bird3 from './images/bird3.jpeg';
+import bird4 from './images/bird4.jpeg';
+import bird5 from './images/bird5.jpeg';
+import bird6 from './images/bird6.jpeg';
+import bird7 from './images/bird7.jpeg';
+import bird8 from './images/bird8.jpeg';
+
+export default [
+    {
+        src: birds,
+        style: {
+            left: '5%',
+            transform: 'rotate(-10deg)'
+        }
+    },
+    {
+        src: bird2,
+        style: {
+            left: '40%',
+            transform: 'rotate(6deg)'
+        }
+    },
+    {
+        src: bird3,
+        style: {
+            right: '15%',
+            transform: 'rotate(-9deg)'
+        }
+    },
+    {
+        src: bird4,
+        style: {
+            left: '8%',
+            top: '28%',
+            transform: 'rotate(8deg)'
+        }
+    },
+    {
+        src: bird5,
+        style: {
+            left: '37%',
+            top: '25%',
+            transform: 'rotate(-12deg)'
+        }
+    },
+    {
+        src: bird6,
+        style: {
+            right: '12%',
+            top: '26%',
+            transform: 'rotate(10deg)'
+        }
+    },
+    {
+        src: bird7,
+        style: {
+            left: '25%',
+            top: '48%',
+            transform: 'rotate(-5deg)'
+
+        }
+    },
+    {
+        src: bird8,
+        style: {
+            right: '25%',
+            top: '52%',
+            transform: 'rotate(16deg)'
+        }
+    }
+];
